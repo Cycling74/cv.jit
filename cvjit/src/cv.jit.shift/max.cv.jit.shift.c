5e78cdfb2a2e93a598c8201065d53147f3330bf0
jovansystem <jovansystem@e4aca8ca-2bea-4dff-ba7f-cb1b8f3c8404>
2010-08-04 04:54:40 +0000

    git-svn-id: http://svn.code.sf.net/p/cvjit/code@18 e4aca8ca-2bea-4dff-ba7f-cb1b8f3c8404

--- cvjit/src/cv.jit.shift/max.cv.jit.shift.c
+++ cvjit/src/cv.jit.shift/max.cv.jit.shift.c
@@ -1,26 +1,26 @@
 /*
-max.cv.jit.shift
-	
-
-Copyright 2010, Jean-Marc Pelletier
-jmp@jmpelletier.com
-
-This file is part of cv.jit.
-
-cv.jit is free software: you can redistribute it and/or modify
-it under the terms of the GNU Lesser General Public License as published 
-by the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-cv.jit is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU Lesser General Public License for more details.
-
-You should have received a copy of the GNU Lesser General Public License
-along with cv.jit.  If not, see <http://www.gnu.org/licenses/>.
-
-*/
+ max.cv.jit.shift
+ 
+ 
+ Copyright 2010, Jean-Marc Pelletier
+ jmp@jmpelletier.com
+ 
+ This file is part of cv.jit.
+ 
+ cv.jit is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Lesser General Public License as published 
+ by the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ cv.jit is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Lesser General Public License for more details.
+ 
+ You should have received a copy of the GNU Lesser General Public License
+ along with cv.jit.  If not, see <http://www.gnu.org/licenses/>.
+ 
+ */
 
 #ifdef __cplusplus
 extern "C" {
@@ -41,7 +41,7 @@ typedef struct _max_cv_jit_shift
 	void			*boxout2;
 	void			*massout;
 	
-	t_atom *av;
+	t_atom			av[8];
 } t_max_cv_jit_shift;
 
 //Function definitions
@@ -58,7 +58,7 @@ void *max_cv_jit_shift_class;
 t_symbol *ps_getbox;
 t_symbol *ps_getrot;
 t_symbol *ps_getmass;
-		 	
+
 #ifdef __cplusplus
 extern "C"
 #endif
@@ -68,14 +68,14 @@ int main(void)
 	
 	cv_jit_shift_init();
 	setup(
-			(t_messlist **)&max_cv_jit_shift_class,	//A pointer to the Max class pointer
-			(method)max_cv_jit_shift_new,			//The constructor function
-			(method)max_cv_jit_shift_free,			//The destructor function
-			(short)sizeof(t_max_cv_jit_shift),		//The size of the Max class
-			0L,											//Use only for GUI objects, null for standard Jitter objects
-			A_GIMME,									//Pass arguments as a list of t_atoms
-			0);											//End of type list
-
+		  (t_messlist **)&max_cv_jit_shift_class,	//A pointer to the Max class pointer
+		  (method)max_cv_jit_shift_new,			//The constructor function
+		  (method)max_cv_jit_shift_free,			//The destructor function
+		  (short)sizeof(t_max_cv_jit_shift),		//The size of the Max class
+		  0L,											//Use only for GUI objects, null for standard Jitter objects
+		  A_GIMME,									//Pass arguments as a list of t_atoms
+		  0);											//End of type list
+	
 	p = max_jit_classex_setup(calcoffset(t_max_cv_jit_shift,obex));	//Setup Max class
 	q = jit_class_findbyname(gensym("cv_jit_shift"));				//Get a pointer to the Jitter object class
 	max_jit_classex_mop_wrap(p,q,MAX_JIT_MOP_FLAGS_OWN_BANG|MAX_JIT_MOP_FLAGS_OWN_OUTPUTMATRIX); //custom bang/outputmatrix 		
@@ -83,7 +83,7 @@ int main(void)
 	max_jit_classex_standard_wrap(p,q,0); 	
  	addmess((method)max_cv_jit_shift_assist, "assist", A_CANT,0);	//Add outlet assistance to object
  	addbang((method)max_cv_jit_shift_bang);
-
+	
 	ps_getbox	= gensym("getbox");
 	ps_getrot	= gensym("getvertices");
 	ps_getmass	= gensym("getmass");
@@ -101,7 +101,7 @@ void *max_cv_jit_shift_new(t_symbol *s, long argc, t_atom *argv)
 {
 	t_max_cv_jit_shift *x;
 	void *o;
-
+	
 	if (x=(t_max_cv_jit_shift *)max_jit_obex_new(max_cv_jit_shift_class,gensym("cv_jit_shift"))) {
 		if (o=jit_object_new(gensym("cv_jit_shift"))) {
 			max_jit_mop_setup_simple(x,o,argc,argv);			
@@ -110,8 +110,6 @@ void *max_cv_jit_shift_new(t_symbol *s, long argc, t_atom *argv)
 			x->massout 	= outlet_new(x,0L);
 			x->boxout2 	= outlet_new(x,0L);  
 			x->boxout 	= outlet_new(x,0L); 
-			
-			x->av = NULL;
 		} else {
 			error("cv.jit.shift: could not allocate object");
 			freeobject((t_object *)x);
@@ -172,10 +170,10 @@ void max_cv_jit_shift_mproc(t_max_cv_jit_shift *x, void *mop)
 	t_jit_err err;
 	
 	if (err=(t_jit_err) jit_object_method(
-		max_jit_obex_jitob_get(x),
-		_jit_sym_matrix_calc,
-		jit_object_method(mop,_jit_sym_getinputlist),
-		jit_object_method(mop,_jit_sym_getoutputlist))) 
+										  max_jit_obex_jitob_get(x),
+										  _jit_sym_matrix_calc,
+										  jit_object_method(mop,_jit_sym_getinputlist),
+										  jit_object_method(mop,_jit_sym_getoutputlist))) 
 	{
 		jit_error_code(x,err); 
 	} else {
@@ -189,18 +187,18 @@ void max_cv_jit_shift_assist(t_max_cv_jit_shift *x, void *b, long m, long a, cha
 		max_jit_mop_assist(x,b,m,a,s);
 	} else { //output
 		switch (a) {
-		case 0:
-			sprintf(s,"(list) New bounding box");
-			break; 			
-		case 1:
-			sprintf(s,"(list) Vertices");
-			break; 			
-		case 2:
-			sprintf(s,"(float) Mass");
-			break; 	
-		case 3:
-			sprintf(s,"dumpout");
-			break; 			
+			case 0:
+				sprintf(s,"(list) New bounding box");
+				break; 			
+			case 1:
+				sprintf(s,"(list) Vertices");
+				break; 			
+			case 2:
+				sprintf(s,"(float) Mass");
+				break; 	
+			case 3:
+				sprintf(s,"dumpout");
+				break; 			
 		}
 	}
 }

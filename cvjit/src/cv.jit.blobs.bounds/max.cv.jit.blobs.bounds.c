6d2d242dccdbbbf4701f8d05c66c364b86e19ed1
jovansystem <jovansystem@e4aca8ca-2bea-4dff-ba7f-cb1b8f3c8404>
2010-10-12 06:12:16 +0000

    git-svn-id: http://svn.code.sf.net/p/cvjit/code@26 e4aca8ca-2bea-4dff-ba7f-cb1b8f3c8404

--- cvjit/src/cv.jit.blobs.bounds/max.cv.jit.blobs.bounds.c
+++ cvjit/src/cv.jit.blobs.bounds/max.cv.jit.blobs.bounds.c
@@ -50,10 +50,11 @@ extern "C"
 int main(void)
 {	
 	void *p,*q;
-	
+	union { void **v_ptr; t_messlist **m_ptr; } alias_ptr;
+	alias_ptr.v_ptr = &max_cv_jit_blobs_bounds_class;
 	cv_jit_blobs_bounds_init();
 	setup(
-			(t_messlist **)&max_cv_jit_blobs_bounds_class,	//A pointer to the Max class pointer
+			alias_ptr.m_ptr,	//A pointer to the Max class pointer
 			(method)max_cv_jit_blobs_bounds_new,			//The constructor function
 			(method)max_cv_jit_blobs_bounds_free,			//The destructor function
 			(short)sizeof(t_max_cv_jit_blobs_bounds),		//The size of the Max class

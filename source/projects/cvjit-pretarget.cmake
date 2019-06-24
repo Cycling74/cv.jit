b756f007e66cbaa6e770bb4d4ab93f1907968e29
Jean-Marc Pelletier <jmp@jmpelletier.com>
2019-06-24 13:42:46 +0900

    Updated Cmake files to support new folder organisation, as well as legacy mode for externals that require functionality only found in the old OpenCV c API.

--- source/projects/cvjit-pretarget.cmake
+++ source/projects/cvjit-pretarget.cmake
@@ -5,29 +5,64 @@ project(${THIS_FOLDER_NAME})
 
 include(${CMAKE_CURRENT_SOURCE_DIR}/../../max-api/script/max-pretarget.cmake)
 
+# Set preprocessor macros
+if (LEGACY) 
+	add_compile_definitions(CVJIT_LEGACY)
+endif()
 
-include_directories( 
-	"${C74_INCLUDES}"
-	${CMAKE_CURRENT_SOURCE_DIR}/../../include
-	${CMAKE_CURRENT_SOURCE_DIR}/../../OpenCVsupport/opencv
-	${CMAKE_CURRENT_SOURCE_DIR}/../../OpenCVsupport
-)
+# Set header directories
+if (LEGACY) 
+	include_directories( 
+		"${C74_INCLUDES}"
+		${CMAKE_CURRENT_SOURCE_DIR}/../../include/
+		${CMAKE_CURRENT_SOURCE_DIR}/../../include/opencv/${OPENCV_VERSION_LEGACY}/
+		${CMAKE_CURRENT_SOURCE_DIR}/../../include/opencv/${OPENCV_VERSION_LEGACY}/opencv/
+	)
+else ()
+	include_directories( 
+		"${C74_INCLUDES}"
+		${CMAKE_CURRENT_SOURCE_DIR}/../../include/
+		${CMAKE_CURRENT_SOURCE_DIR}/../../include/opencv/${OPENCV_VERSION_CURRENT}/
+	)
+endif()
 
+# Set extra libraries
 if(APPLE)
-	set(EXTRA_LIBS 
-		${CMAKE_CURRENT_SOURCE_DIR}/../../lib/libcv.a
-		${CMAKE_CURRENT_SOURCE_DIR}/../../lib/libz.a
+	set(STATIC_LIB_EXT a)
+elseif(WIN32)
+	set(STATIC_LIB_EXT lib)
+endif()
+
+set(LIB_DIR ${CMAKE_CURRENT_SOURCE_DIR}/../../lib)
+if (LEGACY)
+	set(OPENCV_LIB_DIR opencv/${OPENCV_VERSION_LEGACY}/x64)
+	string(REPLACE "." "" OPENCV_LIB_SUFFIX "${OPENCV_VERSION_LEGACY}")
+
+	set(EXTRA_LIBS
+		optimized ${LIB_DIR}/${OPENCV_LIB_DIR}/opencv_core${OPENCV_LIB_SUFFIX}.${STATIC_LIB_EXT}
+		debug ${LIB_DIR}/${OPENCV_LIB_DIR}/opencv_core${OPENCV_LIB_SUFFIX}d.${STATIC_LIB_EXT}
+		optimized ${LIB_DIR}/${OPENCV_LIB_DIR}/opencv_features2d${OPENCV_LIB_SUFFIX}.${STATIC_LIB_EXT}
+		debug ${LIB_DIR}/${OPENCV_LIB_DIR}/opencv_features2d${OPENCV_LIB_SUFFIX}d.${STATIC_LIB_EXT}
+		optimized ${LIB_DIR}/${OPENCV_LIB_DIR}/opencv_imgproc${OPENCV_LIB_SUFFIX}.${STATIC_LIB_EXT}
+		debug ${LIB_DIR}/${OPENCV_LIB_DIR}/opencv_imgproc${OPENCV_LIB_SUFFIX}d.${STATIC_LIB_EXT}
+		optimized ${LIB_DIR}/${OPENCV_LIB_DIR}/opencv_calib3d${OPENCV_LIB_SUFFIX}.${STATIC_LIB_EXT}
+		debug ${LIB_DIR}/${OPENCV_LIB_DIR}/opencv_calib3d${OPENCV_LIB_SUFFIX}d.${STATIC_LIB_EXT}
+		optimized ${LIB_DIR}/${OPENCV_LIB_DIR}/opencv_video${OPENCV_LIB_SUFFIX}.${STATIC_LIB_EXT}
+		optimized ${LIB_DIR}/${OPENCV_LIB_DIR}/opencv_legacy${OPENCV_LIB_SUFFIX}.${STATIC_LIB_EXT}
+		debug ${LIB_DIR}/${OPENCV_LIB_DIR}/opencv_legacy${OPENCV_LIB_SUFFIX}d.${STATIC_LIB_EXT}
+
+		optimized ${LIB_DIR}/zlib/x64/zlibstatic.${STATIC_LIB_EXT}
+		debug ${LIB_DIR}/zlib/x64/zlibstaticd.${STATIC_LIB_EXT}
 	)
 else()
-	if(WIN64)
-		set(EXTRA_LIBS
-			${CMAKE_CURRENT_SOURCE_DIR}/../../lib/x64/opencv.lib
-			${CMAKE_CURRENT_SOURCE_DIR}/../../lib/x64/zlib.lib
-		)
-	else ()
-		set(EXTRA_LIBS
-			${CMAKE_CURRENT_SOURCE_DIR}/../../lib/opencv.lib
-			${CMAKE_CURRENT_SOURCE_DIR}/../../lib/zlib.lib
-		)
-	endif ()
+	set(OPENCV_LIB_DIR opencv/${OPENCV_VERSION_CURRENT}/x64)
+	string(REPLACE "." "" OPENCV_LIB_SUFFIX "${OPENCV_VERSION_CURRENT}")
+
+	set(EXTRA_LIBS
+		optimized ${LIB_DIR}/${OPENCV_LIB_DIR}/opencv_world${OPENCV_LIB_SUFFIX}.${STATIC_LIB_EXT}
+		debug ${LIB_DIR}/${OPENCV_LIB_DIR}/opencv_world${OPENCV_LIB_SUFFIX}d.${STATIC_LIB_EXT}
+
+		optimized ${LIB_DIR}/zlib/x64/zlibstatic.${STATIC_LIB_EXT}
+		debug ${LIB_DIR}/zlib/x64/zlibstaticd.${STATIC_LIB_EXT}
+	)	
 endif()

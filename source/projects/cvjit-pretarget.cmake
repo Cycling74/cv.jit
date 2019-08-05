21c93e61a75049763b060db354106e94a9d7f4cc
Jean-Marc Pelletier <jmp@jmpelletier.com>
2019-08-05 22:40:24 +0900

    Changed OpenCV libs

--- source/projects/cvjit-pretarget.cmake
+++ source/projects/cvjit-pretarget.cmake
@@ -53,8 +53,65 @@ endif()
 
 ## OpenCV libraries
 if (LEGACY)
-	set(OPENCV_RELEASE_LIBS ${OPENCV_LEGACY_RELEASE_LIBS} ${OPENCV_LEGACY_3RD_PARTY_RELEASE_LIBS})
-	set(OPENCV_DEBUG_LIBS ${OPENCV_LEGACY_DEBUG_LIBS} ${OPENCV_LEGACY_3RD_PARTY_DEBUG_LIBS})
+	# set(OPENCV_RELEASE_LIBS ${OPENCV_LEGACY_RELEASE_LIBS} ${OPENCV_LEGACY_3RD_PARTY_RELEASE_LIBS})
+	# set(OPENCV_DEBUG_LIBS ${OPENCV_LEGACY_DEBUG_LIBS} ${OPENCV_LEGACY_3RD_PARTY_DEBUG_LIBS})
+	if (WIN32)
+		set(OPENCV_RELEASE_LIBS
+			"${OPENCV_LEGACY_LIB_DIR}/IlmImf.lib"
+			"${OPENCV_LEGACY_LIB_DIR}/libjasper.lib"
+			"${OPENCV_LEGACY_LIB_DIR}/libjpeg.lib"
+			"${OPENCV_LEGACY_LIB_DIR}/libpng.lib"
+			"${OPENCV_LEGACY_LIB_DIR}/libtiff.lib"
+			"${OPENCV_LEGACY_LIB_DIR}/opencv_calib3d2412.lib"
+			"${OPENCV_LEGACY_LIB_DIR}/opencv_contrib2412.lib"
+			"${OPENCV_LEGACY_LIB_DIR}/opencv_core2412.lib"
+			"${OPENCV_LEGACY_LIB_DIR}/opencv_features2d2412.lib"
+			"${OPENCV_LEGACY_LIB_DIR}/opencv_flann2412.lib"
+			"${OPENCV_LEGACY_LIB_DIR}/opencv_gpu2412.lib"
+			"${OPENCV_LEGACY_LIB_DIR}/opencv_highgui2412.lib"
+			"${OPENCV_LEGACY_LIB_DIR}/opencv_imgproc2412.lib"
+			"${OPENCV_LEGACY_LIB_DIR}/opencv_legacy2412.lib"
+			"${OPENCV_LEGACY_LIB_DIR}/opencv_ml2412.lib"
+			"${OPENCV_LEGACY_LIB_DIR}/opencv_nonfree2412.lib"
+			"${OPENCV_LEGACY_LIB_DIR}/opencv_objdetect2412.lib"
+			"${OPENCV_LEGACY_LIB_DIR}/opencv_ocl2412.lib"
+			"${OPENCV_LEGACY_LIB_DIR}/opencv_photo2412.lib"
+			"${OPENCV_LEGACY_LIB_DIR}/opencv_stitching2412.lib"
+			"${OPENCV_LEGACY_LIB_DIR}/opencv_superres2412.lib"
+			"${OPENCV_LEGACY_LIB_DIR}/opencv_ts2412.lib"
+			"${OPENCV_LEGACY_LIB_DIR}/opencv_video2412.lib"
+			"${OPENCV_LEGACY_LIB_DIR}/opencv_videostab2412.lib"
+			"${OPENCV_LEGACY_LIB_DIR}/zlib.lib"
+		)
+
+		set(OPENCV_DEBUG_LIBS
+			"${OPENCV_LEGACY_LIB_DIR}/IlmImfd.lib"
+			"${OPENCV_LEGACY_LIB_DIR}/libjasperd.lib"
+			"${OPENCV_LEGACY_LIB_DIR}/libjpegd.lib"
+			"${OPENCV_LEGACY_LIB_DIR}/libpngd.lib"
+			"${OPENCV_LEGACY_LIB_DIR}/libtiffd.lib"
+			"${OPENCV_LEGACY_LIB_DIR}/opencv_calib3d2412d.lib"
+			"${OPENCV_LEGACY_LIB_DIR}/opencv_contrib2412d.lib"
+			"${OPENCV_LEGACY_LIB_DIR}/opencv_core2412d.lib"
+			"${OPENCV_LEGACY_LIB_DIR}/opencv_features2d2412d.lib"
+			"${OPENCV_LEGACY_LIB_DIR}/opencv_flann2412d.lib"
+			"${OPENCV_LEGACY_LIB_DIR}/opencv_gpu2412d.lib"
+			"${OPENCV_LEGACY_LIB_DIR}/opencv_highgui2412d.lib"
+			"${OPENCV_LEGACY_LIB_DIR}/opencv_imgproc2412d.lib"
+			"${OPENCV_LEGACY_LIB_DIR}/opencv_legacy2412d.lib"
+			"${OPENCV_LEGACY_LIB_DIR}/opencv_ml2412d.lib"
+			"${OPENCV_LEGACY_LIB_DIR}/opencv_nonfree2412d.lib"
+			"${OPENCV_LEGACY_LIB_DIR}/opencv_objdetect2412d.lib"
+			"${OPENCV_LEGACY_LIB_DIR}/opencv_ocl2412d.lib"
+			"${OPENCV_LEGACY_LIB_DIR}/opencv_photo2412d.lib"
+			"${OPENCV_LEGACY_LIB_DIR}/opencv_stitching2412d.lib"
+			"${OPENCV_LEGACY_LIB_DIR}/opencv_superres2412d.lib"
+			"${OPENCV_LEGACY_LIB_DIR}/opencv_ts2412d.lib"
+			"${OPENCV_LEGACY_LIB_DIR}/opencv_video2412d.lib"
+			"${OPENCV_LEGACY_LIB_DIR}/opencv_videostab2412d.lib"
+			"${OPENCV_LEGACY_LIB_DIR}/zlibd.lib"
+		)
+	endif()
 else()
 	set(OPENCV_RELEASE_LIBS ${OPENCV_LATEST_RELEASE_LIBS})
 	set(OPENCV_DEBUG_LIBS ${OPENCV_LATEST_DEBUG_LIBS})

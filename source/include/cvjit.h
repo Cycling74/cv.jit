3a97808092887c8d71355f93e86bbabecbb6b172
Jean-Marc Pelletier <jmp@jmpelletier.com>
2020-03-06 22:44:12 +0900

    Added some utility classes and functions but these are mostly still a work in progress.

--- source/include/cvjit.h
+++ source/include/cvjit.h
@@ -47,6 +47,11 @@ namespace cvjit {
 	/* Useful type definitions */
 	using cstring = char const * const;
 
+	template <typename T>
+	inline T rad2deg(T rad) {
+		return rad * (T)57.295779513082320876798154814105;
+	}
+
 	class Flags {
 	public:
 		static constexpr long get_set = ATTR_GET_DEFER_LOW | ATTR_SET_USURP_LOW;
@@ -390,6 +395,65 @@ namespace cvjit {
 
 #define CVJIT_CALCOFFSET(member) (cvjit::member_offset<decltype(cvjit::get_class_type(member)), decltype(cvjit::get_member_type(member)), member>())
 
+	struct AttributeAlias {
+		t_symbol * sym;
+		t_atom_long val;
+		t_atom atom;
+
+		AttributeAlias(t_symbol * s, t_atom_long v) : sym(s), val(v) {
+			atom_setsym(&atom, s);
+		}
+
+		template <typename T>
+		T as() const {
+			return static_cast<T>(val);
+		}
+
+		void get(long *ac, t_atom **av) {
+			if (*ac && *av) {
+				atom_setsym(*av, sym);
+				*ac = 1;
+			}
+			else {
+				*ac = 1;
+				*av = &atom;
+			}
+		}
+	};
+
+	template <typename T>
+	struct AttributeAliases {
+	private:
+		static constexpr std::size_t count = (std::size_t)T::COUNT;
+		t_symbol * m_symbols[count];
+	public:
+		template <typename... Args>
+		AttributeAliases(std::pair<T, cvjit::cstring> pair, Args... args) {
+			static_assert(sizeof...(Args) + 1 == (std::size_t)count, "Incorrect number of arguments");
+			std::array <std::pair <T, cvjit::cstring>, count> arr = { pair, args... };
+			for (int i = 0; i < count; i++) {
+				m_symbols[(std::size_t)arr[i].first] = gensym(arr[i].second);
+			}
+		}
+
+		AttributeAlias get() const {
+			return { m_symbols[0], 0 };
+		}
+
+		AttributeAlias get(t_atom * a) const {
+			if (a->a_type == A_SYM) {
+				for (t_atom_long i = 0; i < (t_atom_long)count; i++) {
+					if (m_symbols[i] == a->a_w.w_sym) {
+						return { m_symbols[i], i };
+					}
+				}
+				return get();
+			}
+			t_atom_long val = clamp(atom_getlong(a), (t_atom_long)0, (t_atom_long)(count - 1));
+			return { m_symbols[val], val };
+		}
+	};
+
 	struct AttributeManager {
 	private:
 		void * m_class;
@@ -453,8 +517,6 @@ namespace cvjit {
 			jit_attr_addfilterset_clip(attr, 0, 1, true, true);
 			jit_class_addattr(m_class, attr);
 		}
-
-
 	};
 
 	template <typename T>
@@ -485,6 +547,148 @@ namespace cvjit {
 			info.dimcount,
 			info.dim[0], info.dim[0],
 			info.type == _jit_sym_char ? "char" : info.type == _jit_sym_long ? "long" : info.type == _jit_sym_float32 ? "float32" : "float64");
+	}
+
+	using max_object_constructor = auto (*)(t_symbol *s, short argc, t_atom *argv) -> void *;
+
+	template <typename T>
+	using max_object_destructor = auto (*)(T *) -> void;
+
+	template <typename T, typename U>
+	inline U new_max_class(U & class_ptr, cvjit::cstring name, max_object_constructor constructor, max_object_destructor<T> destructor)
+	{
+		t_class	*c = class_new(name,
+			(method)constructor,
+			(method)destructor,
+			sizeof(T),
+			(method)NULL,
+			A_GIMME,
+			0);
+
+		static_assert(std::is_same<U, void *>::value || std::is_same<U, t_class *>::value, "Class must be void * or t_class *");
+		union { void **v_ptr; t_messlist **m_ptr; } alias_ptr;
+		alias_ptr.v_ptr = &(void *)class_ptr;
+		setup(alias_ptr.m_ptr, (method)constructor, (method)destructor, (short)sizeof(T), 0L, A_GIMME, 0);
+		return class_ptr;
+	}
+
+	template <typename T>
+	using bang_callback = auto (*)(T *) -> void;
+
+	template <typename T>
+	using assist_callback = auto (*)(T *, void *, long, long, char *) -> void;
+
+	class ClassGenerator {
+	private:
+		t_class * m_class{nullptr};
+	public:
+		template <typename T>
+		ClassGenerator(cvjit::cstring name, max_object_constructor constructor, max_object_destructor<T> destructor) {
+			m_class = class_new(name,
+				(method)constructor,
+				(method)destructor,
+				sizeof(T),
+				(method)NULL,
+				A_GIMME,
+				0);
+		}
+
+		~ClassGenerator() {
+			class_register(CLASS_BOX, m_class);
+		}
+
+		template <typename T>
+		void set_bang(bang_callback<T> callback) {
+			class_addmethod(m_class, (method)callback, "bang", 0);
+		}
+
+		template <typename T>
+		void set_assist(assist_callback<T> callback) {
+			class_addmethod(m_class, (method)callback, "assist", A_CANT, 0);
+		}
+
+		t_class * get_ptr() const { return m_class; }
+	};
+
+	
+
+	template <typename T, typename U>
+	inline void set_bang(U class_ptr, bang_callback<T> callback)
+	{
+		addmess((method)callback, "bang", A_CANT, 0);
+	}
+
+	template <typename T>
+	inline void set_bang(bang_callback<T> callback)
+	{
+		addmess((method)callback, "bang", A_CANT, 0);
+	}
+
+	template <typename T, typename U>
+	inline void set_assist(U class_ptr, assist_callback<T> callback)
+	{
+		addmess((method)callback, "assist", A_CANT, 0);
+	}
+
+	template <typename T>
+	inline void set_assist(assist_callback<T> callback)
+	{
+		addmess((method)callback, "assist", A_CANT, 0);
+	}
+
+	template <typename T>
+	inline void to_atom(T const & v, t_atom * a) {
+		T::unimplemented;
+	}
+
+	template <>
+	inline void to_atom<t_atom>(t_atom const & v, t_atom * a) {
+		*a = v;
+	}
+
+	template <>
+	inline void to_atom<t_atom_long>(t_atom_long const & v, t_atom * a) {
+		atom_setlong(a, v);
+	}
+
+	template <>
+	inline void to_atom<t_atom_float>(t_atom_float const & v, t_atom * a) {
+		atom_setfloat(a, v);
+	}
+
+	template <>
+	inline void to_atom<t_symbol *>(t_symbol * const & v, t_atom * a) {
+		atom_setsym(a, v);
+	}
+
+	template <typename T>
+	void attribute_get(std::vector<T> & data_src, short *argc, t_atom **argv)
+	{
+		const short count = static_cast<short>(std::min(static_cast<std::size_t>(std::numeric_limits<short>::max()), data_src.size()));
+		t_atom * ptr = nullptr;
+		if (*argc && *argv) {
+			if (*argc != count) {
+				ptr = (t_atom *)sysmem_resizeptr(*argv, sizeof(t_atom) * count);
+			}
+			else {
+				ptr = *argv;
+			}
+		}
+		else {
+			ptr = (t_atom *)sysmem_newptr(sizeof(t_atom) * count);
+		}
+
+		if (!ptr) {
+			object_error(nullptr, "Out of memory!");
+			return;
+		}
+
+		for (std::size_t i = 0; i < count; i++) {
+			to_atom(data_src[i], ptr + i);
+		}
+
+		*argv = ptr;
+		*argc = (short)count;
 	}
 
 }
